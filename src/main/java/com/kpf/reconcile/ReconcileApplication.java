9bbc0f268d6341a04268405f6b358c3be72bc3e3
Krupesh <kfaldu@loyalty.com>
2018-09-30 12:21:24 -0400

    udpate bank list with proper amount

--- src/main/java/com/kpf/reconcile/ReconcileApplication.java
+++ src/main/java/com/kpf/reconcile/ReconcileApplication.java
@@ -6,6 +6,7 @@ import com.fasterxml.jackson.dataformat.csv.CsvSchema;
 import java.io.File;
 import java.util.Collections;
 import java.util.List;
+import java.util.stream.Collectors;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -15,28 +16,38 @@ import org.springframework.core.io.ClassPathResource;
 @Slf4j
 public class ReconcileApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(ReconcileApplication.class, args);
+  public static void main(String[] args) {
+    SpringApplication.run(ReconcileApplication.class, args);
 
     List<GL> glList = loadObjectList(GL.class, "gl.csv");
-    System.out.println(glList);
-
-    List<Bank> bankList = loadObjectList(Bank.class, "bank.csv");
-    System.out.println(bankList);
-
+    List<Bank> bankList = updateBankList(loadObjectList(Bank.class, "bank.csv"));
   }
 
-	public static <T> List<T> loadObjectList(Class<T> type, String fileName) {
-		try {
-			CsvSchema bootstrapSchema = CsvSchema.emptySchema().withHeader();
-			CsvMapper mapper = new CsvMapper();
-			File file = new ClassPathResource(fileName).getFile();
-			MappingIterator<T> readValues =
-					mapper.reader(type).with(bootstrapSchema).readValues(file);
-			return readValues.readAll();
-		} catch (Exception e) {
-			log.error("Error occurred while loading object list from file " + fileName, e);
-			return Collections.emptyList();
-		}
-	}
+  public static List<Bank> updateBankList(List<Bank> bankList) {
+    return bankList.stream().map(bank -> {
+      if (bank.debit != null) {
+        bank.amount = bank.debit * -1;
+      } else if (bank.credit != null) {
+        bank.amount = bank.credit;
+      }
+      return bank;
+    }).collect(Collectors.toList());
+  }
+
+  public static void getPerfectMatch(List<Bank> bankList, List<GL> glList) {
+  }
+
+  public static <T> List<T> loadObjectList(Class<T> type, String fileName) {
+    try {
+      CsvSchema bootstrapSchema = CsvSchema.emptySchema().withHeader();
+      CsvMapper mapper = new CsvMapper();
+      File file = new ClassPathResource(fileName).getFile();
+      MappingIterator<T> readValues =
+          mapper.reader(type).with(bootstrapSchema).readValues(file);
+      return readValues.readAll();
+    } catch (Exception e) {
+      log.error("Error occurred while loading object list from file " + fileName, e);
+      return Collections.emptyList();
+    }
+  }
 }
