2a671b2e5940e47ce630209cf84462d87b8dceaa
Krupesh <kfaldu@loyalty.com>
2018-09-30 12:02:54 -0400

    read from csv file

---
+++ src/main/java/com/kpf/reconcile/GL.java
@@ -0,0 +1,14 @@
+package com.kpf.reconcile;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class GL {
+  String description;
+  Double amount;
+  Long entryNumber;
+}
--- src/main/java/com/kpf/reconcile/ReconcileApplication.java
+++ src/main/java/com/kpf/reconcile/ReconcileApplication.java
@@ -1,12 +1,37 @@
 package com.kpf.reconcile;
 
+import com.fasterxml.jackson.databind.MappingIterator;
+import com.fasterxml.jackson.dataformat.csv.CsvMapper;
+import com.fasterxml.jackson.dataformat.csv.CsvSchema;
+import java.io.File;
+import java.util.Collections;
+import java.util.List;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.core.io.ClassPathResource;
 
 @SpringBootApplication
+@Slf4j
 public class ReconcileApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ReconcileApplication.class, args);
+    List<GL> glList = loadObjectList(GL.class, "gl.csv");
+    System.out.println(glList);
+  }
+
+	public static <T> List<T> loadObjectList(Class<T> type, String fileName) {
+		try {
+			CsvSchema bootstrapSchema = CsvSchema.emptySchema().withHeader();
+			CsvMapper mapper = new CsvMapper();
+			File file = new ClassPathResource(fileName).getFile();
+			MappingIterator<T> readValues =
+					mapper.reader(type).with(bootstrapSchema).readValues(file);
+			return readValues.readAll();
+		} catch (Exception e) {
+			log.error("Error occurred while loading object list from file " + fileName, e);
+			return Collections.emptyList();
+		}
 	}
 }
